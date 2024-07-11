6f7315a63291783af9e291bfac46a87775642664
ovaliente <ovaliente@frba.utn.edu.ar>
2024-07-11 15:04:15 +0000

    Test Salvation Edge

--- compilator.sh
+++ compilator.sh
@@ -16,4 +16,35 @@ sleep 2
 x-terminal-emulator -e "bash -c 'cd kernel && make memcheck; exec bash'" &
 
 # Esperar unos segundos para dar tiempo a que se abra la tercera terminal
-sleep 5
+sleep 4
+
+# Navegar a la carpeta 'io' y ejecutar el comando make memcheck en una cuarta terminal, luego abrir un bash interactivo
+x-terminal-emulator -e "bash -c 'cd entradasalida && make && ./bin/entradasalida MONITOR ../entradasalida/io_stdout.config; exec bash'" &
+
+# Esperar unos segundos para dar tiempo a que se abra la cuarta terminal
+sleep 2
+
+# (Parece que hay un duplicado, eliminé la segunda repetición para la carpeta 'io')
+# Navegar a la carpeta 'io' y ejecutar el comando make memcheck en una cuarta terminal, luego abrir un bash interactivo
+x-terminal-emulator -e "bash -c 'cd entradasalida && make && ./bin/entradasalida TECLADO ../entradasalida/io_stdin.config; exec bash'" &
+
+# Esperar unos segundos para dar tiempo a que se abra la cuarta terminal
+sleep 2
+
+# (Parece que hay un duplicado, eliminé la segunda repetición para la carpeta 'io')
+# Navegar a la carpeta 'io' y ejecutar el comando make memcheck en una cuarta terminal, luego abrir un bash interactivo
+x-terminal-emulator -e "bash -c 'cd entradasalida && make && ./bin/entradasalida GENERICA ../entradasalida/generica.config; exec bash'" &
+
+# Esperar unos segundos para dar tiempo a que se abra la cuarta terminal
+sleep 2
+
+# (Parece que hay un duplicado, eliminé la segunda repetición para la carpeta 'io')
+# Navegar a la carpeta 'io' y ejecutar el comando make memcheck en una cuarta terminal, luego abrir un bash interactivo
+x-terminal-emulator -e "bash -c 'cd entradasalida && make && ./bin/entradasalida SLP1 ../entradasalida/slp1.config; exec bash'" &
+
+# Esperar unos segundos para dar tiempo a que se abra la cuarta terminal
+sleep 2
+
+# (Parece que hay un duplicado, eliminé la segunda repetición para la carpeta 'io')
+# Navegar a la carpeta 'io' y ejecutar el comando make memcheck en una cuarta terminal, luego abrir un bash interactivo
+x-terminal-emulator -e "bash -c 'cd entradasalida && make && ./bin/entradasalida ESPERA ../entradasalida/espera.config; exec bash'" &
